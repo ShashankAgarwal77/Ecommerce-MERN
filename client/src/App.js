c816c2e4784ee49988e2d2760f0a0feb2a32b0c2
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-07-20 01:13:19 +0530

    adding react router dom for home, login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <p>react app</p>
-  </div>
-);
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
+  );
+};
+
 export default App;
