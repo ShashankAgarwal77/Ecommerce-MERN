e42a7b18c71aab14b2104b4801cc1dcbae0748c3
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-07-21 23:11:42 +0530

    adding firebase.js and designing register page

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
-import { Menu } from 'antd';
+import React, { useState } from "react";
+import { Menu } from "antd";
 import {
   MailOutlined,
   AppstoreOutlined,
   SettingOutlined,
   UserOutlined,
   UserAddOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
+
+import { Link } from "react-router-dom";
+
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState('home');
+  const [current, setCurrent] = useState("home");
 
   const handleClick = (e) => {
     // console.log(e.key);
@@ -18,23 +21,23 @@ const Header = () => {
   };
 
   return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
-      <Item key='home' icon={<AppstoreOutlined />}>
-        Home
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
       </Item>
 
-      <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Username'>
-        <Item key='setting:1'>Option 1</Item>
-        <Item key='setting:2'>Option 2</Item>
+      <Item key="register" icon={<UserAddOutlined />} className="float-right">
+        <Link to="/register">Register</Link>
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
+        <Link to="/login">Login</Link>
+      </Item>
+
+      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
       </SubMenu>
-
-      <Item key='login' icon={<UserOutlined />} className='float-right'>
-        Login
-      </Item>
-
-      <Item key='register' icon={<UserAddOutlined />}>
-        Register
-      </Item>
     </Menu>
   );
 };
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import { MDBInput } from "mdb-react-ui-kit";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    //
+  };
+
+  const registerForm = () => {};
+
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          <form onSubmit={handleSubmit}>
+            <MDBInput
+              label="Enter Email Address"
+              type="email"
+              id="typeEmail"
+              // className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autofocus
+            />
+
+            <button type="submit" className="btn btn-raised">
+              Register / {email}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
