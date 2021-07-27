2d297c208c9db022c83b076db7d55e444514df93
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-07-27 23:42:13 +0530

    logout has been set

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -8,15 +8,29 @@ import {
 } from "@ant-design/icons";
 
 import { Link } from "react-router-dom";
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
+  let dispatch = useDispatch();
+  let history = useHistory();
 
   const handleClick = (e) => {
     // console.log(e.key);
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.pushState("/login");
   };
 
   return (
@@ -36,6 +50,9 @@ const Header = () => {
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
+        <Item icon={<UserOutlined />} onClick={logout}>
+          Logout
+        </Item>
       </SubMenu>
     </Menu>
   );
