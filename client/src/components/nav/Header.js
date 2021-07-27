66c10696e67e05d27e84e42297771919a1f39064
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-07-27 23:42:59 +0530

    logout feature has been added

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -5,6 +5,7 @@ import {
   SettingOutlined,
   UserOutlined,
   UserAddOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
 
 import { Link } from "react-router-dom";
@@ -50,7 +51,7 @@ const Header = () => {
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
-        <Item icon={<UserOutlined />} onClick={logout}>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
           Logout
         </Item>
       </SubMenu>
