cfd72ddd6cfbaf2b17729aae0cbea8c5ded126bc
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-07-28 22:37:11 +0530

    register with google added and some features in header.js

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
-import { MDBInput } from "mdb-react-ui-kit";
-
-const Register = () => {
+const Register = ({ history }) => {
   const [email, setEmail] = useState("");
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  // useEffect(() => {
+  //   if (user && user.token) history.push("/");
+  // }, [user]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,34 +24,36 @@ const Register = () => {
     toast.success(
       `Email is sent to ${email}. Click the link to complete your registration.`,
     );
-
-    //save user email in local storage
+    // save user email in local storage
     window.localStorage.setItem("emailForRegistration", email);
-
-    //clear the state
+    // clear state
     setEmail("");
   };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Your email"
+        autoFocus
+      />
+
+      <br />
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
 
   return (
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
-          <form onSubmit={handleSubmit}>
-            <MDBInput
-              label="Enter Email Address"
-              type="email"
-              id="typeEmail"
-              className="form-control my-4"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoFocus
-            />
-
-            <button type="submit" className="btn btn-raised">
-              Register
-            </button>
-          </form>
+          {registerForm()}
         </div>
       </div>
     </div>
